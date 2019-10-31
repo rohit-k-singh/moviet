af646b54060d2ad7f8be81dff75cf2a3f895abd4
Rocodes <Rksingh620@gmail.com>
2019-10-31 08:34:04 +0530

    Major changes

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
-var card = document.getElementById("cardbox");
+var card = document.getElementById("xx");
 var searchVal;
 var first = "";
 var url = `https://api.themoviedb.org/3/discover/movie?api_key=9ca321e0477eb3e89ec7f2b59812c219&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
 document.getElementById("dataTitle").innerHTML = "Popular Movies";
 fetchit(url);
 function func() {
-  searchVal = document.getElementById('search').value;
+  searchVal = document.getElementById('searchterm').value;
   console.log(searchVal);
    url = `https://api.themoviedb.org/3/search/movie?api_key=9ca321e0477eb3e89ec7f2b59812c219&language=en-US&query=${searchVal}`;
+  fetchit(url)
+}
+function tvpage(){
+  url = `https://api.themoviedb.org/3/discover/tv?api_key=9ca321e0477eb3e89ec7f2b59812c219&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false`;
   fetchit(url)
 }
 function fetchit(url) {
@@ -19,62 +23,46 @@ function fetchit(url) {
     });
 }
 function createCard(first) {
-  // console.log(data);
   card.innerHTML = "";
   first.forEach(element => {
-    var summary = element.overview;
-    summary = summary.substr(0, 300);
-    var cardData = `
-                    <div class="card">
-                    <div class="face face1">
-                      <div class="content">
-                        <img src="https://image.tmdb.org/t/p/w500${element.poster_path}" id="iimg" alt="">
-                      </div>
-                    </div>
-                    <div class="face face2">
-                      <div class="content">
-                        <p>
-                        ${summary}
-                        </p>
-                        <button id="${element.id}"  class="searchbtn" onclick="getButtonID(this.id)">Get details</button>
-                      </div>
-                    </div>
-                  </div>`;
-    card.innerHTML += cardData;
-    //     var value1=element.id;
-    // var queryString = "?para1=" + value1;
-    // window.location.href = "details.html" + queryString;
+      var summary = element.overview;
+      var url = 'https://image.tmdb.org/t/p/w500'+element.poster_path;
+      summary = summary.substr(0, 110);
+      var cardData =
+        `<div class="containerr" style="background-image:url(${url});">
+        <div class="overlay"><div class = "items title">${element.title}</div>
+        <div class = "items head"><p>${summary}</p></div>
+        <div class = "items price"><p class="new">Rating:${element.vote_average}\/10</p>
+        </div><div class="items cart"><i class="fa fa-shopping-cart"></i><div>${element.release_date}</div>
+        <button class="cardbtn"><a href="details.jsp?userId=${element.id}">View More</a></button></div>
+          </div>
+        </div>`;
 
+      card.innerHTML += cardData;
   });
-
 }
-// function getButtonID(id){
-//   var value1=element.id;
-//   var queryString = "?para1=" + value1;
-//   window.location.href = "details.html" + queryString;
-// }
+
 function scheduleByName(sortterm) {
-  // console.log(first);
-  var byName = first.slice(0);
-  byName.sort(function (a, b) {
-    var x = a.title.toLowerCase();
-    var y = b.title.toLowerCase();
-    return x < y ? -1 : x > y ? 1 : 0;
+  var JSONdata = first.slice(0);
+  if(sortterm == "Name"){
+    JSONdata.sort(function (a, b) {
+      console.log(a,b);
+      var x = a.title.toLowerCase();
+      var y = b.title.toLowerCase();
+      return x < y ? -1 : x > y ? 1 : 0;
+    });
+  }
+  else if(sortterm == "Rating"){
+            JSONdata.sort(function(a,b) {
+                return b.vote_average - a.vote_average;
+            });
+        }
+  else if(sortterm == "Date"){
+    JSONdata.sort(function(a, b) {
+      a = new Date(a.release_date);
+      b = new Date(b.release_date);
+      return a>b ? -1 : a<b ? 1 : 0;
   });
-  // console.log('by name:');
-  // console.log(byName);
-  createCard(byName);
+        }
+  createCard(JSONdata);
 }
-// to send data to next page
-// var value1=element.id;
-// var queryString = "?para1=" + value1;
-// window.location.href = "details.html" + queryString;
-
-// put this in next page script to recover data
-// var queryString = decodeURIComponent(window.location.search);
-// queryString = queryString.substring(1);
-// var queries = queryString.split("&");
-// for (var i = 0; i < queries.length; i++)
-// {
-//   document.write(queries[i] + "<br>");
-// }
